bcef7333b070e80094113523a2e28aec7d9d16e6
Marcus Daly <daly.marcus@gmail.com>
2015-12-14 18:30:13 +0000

    Balls now generate correctly

--- Game Engines Assignment/Assets/AccelerometerInput.cs
+++ Game Engines Assignment/Assets/AccelerometerInput.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class AccelerometerInput : MonoBehaviour {
     public float speed = 1.0f;
 	
-	// Update is called once per frame
+	// Use the gyroscope to move the balls about
+    //needs force instead of transforms to make phsyics work
 	void Update () {
         float moveX = (Input.acceleration.x * speed);
         float moveY = (Input.acceleration.y * speed);
         transform.Translate(moveX, 0, moveY);
+
+        //Physics.gravity = new Vector3(moveX, 0, moveY);
     }
 }
--- Game Engines Assignment/Assets/Generator.cs
+++ Game Engines Assignment/Assets/Generator.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 
 public class Generator : MonoBehaviour {
     public GameObject test;
-	// Use this for initialization
+	//ball creation algorithm
+    //needs to create balls in a triangle
 	void Start () {
+        //
+        int z = 5;
+        float offset = 0;
+
+        //begin loop to generate triangle of balls
         for (int i = 0; i < 5; i++)
         {
+            for(float j = 0; j < i+1; j++)
+            {
+                
+                createBall(j - offset, z);
+            }
+            z++;
+            offset += 0.5f;
             
-            int locationX = Random.Range(-3, 3);
-            int locationY = Random.Range(-7, -13);
-            Instantiate(test);
-            //test.speed = 0.5f;
-            test.transform.position = new Vector3(locationX, 1, locationY);
         }
     }
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	//ball create function
+    //creates a ball, makes it a rigid body, gives it a location
+	void createBall(float x, int z)
+    {
+        Instantiate(test);
+        test.GetComponent<Rigidbody>();
+        //test.Rigidbody.mass = 0.5f;
+        test.transform.position = new Vector3(x, 1, z);
+    }
 }
