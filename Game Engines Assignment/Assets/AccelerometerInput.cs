e414bee209d16fa8af374b7eac91f4a32974cc0c
Marcus Daly <daly.marcus@gmail.com>
2015-12-14 16:14:36 +0000

    Ball generation
    Testing generating balls as well as physics with gyroscope controls

--- Game Engines Assignment/Assets/AccelerometerInput.cs
+++ Game Engines Assignment/Assets/AccelerometerInput.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class AccelerometerInput : MonoBehaviour {
-
+    public float speed = 1.0f;
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
-	}
+        float moveX = (Input.acceleration.x * speed);
+        float moveY = (Input.acceleration.y * speed);
+        transform.Translate(moveX, 0, moveY);
+    }
 }
---
+++ Game Engines Assignment/Assets/Generator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Generator : MonoBehaviour {
+    public GameObject test;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 5; i++)
+        {
+            
+            int locationX = Random.Range(-3, 3);
+            int locationY = Random.Range(-7, -13);
+            Instantiate(test);
+            //test.speed = 0.5f;
+            test.transform.position = new Vector3(locationX, 1, locationY);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
